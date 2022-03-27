934ed255d87b9052960f1a43984a3cf1e56e2c96
lfy-12 <18318152245@163.com>
2022-03-28 00:10:12 +0800

    加油啊!别偷懒了啊!

--- helper-miniprogram/src/pages/category/index.tsx
+++ helper-miniprogram/src/pages/category/index.tsx
@@ -1,64 +1,108 @@
 import { Tabs, Calendar, TreeSelect, DropdownMenu } from "@taroify/core"
 import { View, Text, ScrollView } from "@tarojs/components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './index.less'
 import { BullhornOutlined } from "@taroify/icons"
+import { request } from '../utils/http'
+import Taro from "@tarojs/taro";
 
-type listType = {
-  id: number,
+type activityListType = {
+  _id: number,
   title: string,
   type: string,
   content: string,
-  pic: string,
   startDate: string,
   endDate: string,
   num: number,
-  comment: Array<string>,
-  community: string
+  community: string,
+  where: string,
+  // comment:[],
+}
+
+type categoryType = {
+  _id: number,
+  value: string,
+  children?: Array<categoryType>
+}
+
+type request = {
+  code: number,
+  data: Array<activityListType | categoryType>
 }
 
 const categoryPage = () => {
   const [value, setValue] = useState<number>(0)
-  const [activityList, setActivityList] = useState<Array<listType>>()
+  const [category, setCategory] = useState<Array<categoryType>>([])
+  const [activityList, setActivityList] = useState<Array<activityListType>>([])
+  const [partActivityList, setPartActivityList] = useState<Array<activityListType>>([])
+
+
+  useEffect(() => {
+    initData()
+  }, [])
+
+  const initData = () => {
+    request({ url: "/category", method: "GET" }).then(res => {
+      setCategory(res.data)
+    })
+    request({ url: "/activity", method: "GET" }).then(res => {
+      setActivityList(res.data)
+    })
+  }
 
   const changeActivityList = (date) => {
     console.log(date);
     // setActivityList
   }
-  return (
-    <>
-      <Tabs value={value} onChange={setValue}>
-        {/* 分类栏 */}
-        <Tabs.TabPane title="TYPE">
-          <View className="cates_container">
-            <ScrollView className="left_menu" scrollY>
-              <View className="menu_type">
-                <Text>/ 活动类别 /</Text>
-                <View className="menu_item" >
-                  
-                </View>
-              </View>
-            </ScrollView>
-          </View>
-        </Tabs.TabPane>
-        {/* 活动日历栏 */}
-        <Tabs.TabPane title="CALENDAR">
-          {/* 日历 */}
-          <Calendar style={{ height: "400px" }} onChange={changeActivityList} title="" defaultValue={new Date()} max={new Date(new Date().setDate(new Date().getDate() + 14))} />
-          {/* 活动列表 */}
-          <View className="lists">
-            <View className="list">
-              <View className="left">
-                <BullhornOutlined />
-                <Text className="title">Title</Text>
-              </View>
-              <Text className="community">Community &gt;</Text>
-            </View>
-          </View>
-        </Tabs.TabPane>
-      </Tabs>
 
-    </>
+  return (
+    <Tabs value={value} onChange={setValue}>
+
+      {/* 分类栏 */}
+      <Tabs.TabPane title="TYPE">
+        <View className="cates_container">
+          <ScrollView className="left_menu" scrollY>
+            {
+              category.map(item => {
+                return (
+                  <View className="menu_type" key={item._id}>
+                    <Text>/ {item.value} /</Text>
+                    <View className="menu_items">
+                      {
+                        item.children?.map(type => {
+                          return (
+                            <View className="menu_item" key={type._id}>{type.value}</View>
+                          )
+                        })
+                      }
+                    </View>
+                  </View>
+                )
+              })
+            }
+          </ScrollView>
+          <ScrollView className="right_menu" scrollY>
+
+          </ScrollView>
+        </View>
+      </Tabs.TabPane>
+
+      {/* 活动日历栏 */}
+      <Tabs.TabPane title="CALENDAR">
+        {/* 日历 */}
+        <Calendar style={{ height: "400px" }} onChange={changeActivityList} title="" defaultValue={new Date()} max={new Date(new Date().setDate(new Date().getDate() + 14))} />
+        {/* 活动列表 */}
+        <View className="lists">
+          <View className="list">
+            <View className="left">
+              <BullhornOutlined />
+              <Text className="title">Title</Text>
+            </View>
+            <Text className="community">Community &gt;</Text>
+          </View>
+        </View>
+      </Tabs.TabPane>
+    </Tabs>
   )
 }
 
---
+++ helper-miniprogram/src/pages/utils/http.ts
@@ -0,0 +1,38 @@
+import Taro from "@tarojs/taro";
+
+let ajaxTimes:number = 0;
+
+type promiseType = {
+    code:number,
+    data:any
+}
+
+export const request = (params) => {
+
+    ajaxTimes++;
+    // 显示加载中 效果
+    Taro.showLoading({
+        title: "加载中",
+        mask: true
+    });
+
+    const baseUrl = 'http://localhost:3000'
+
+    // params包括url、method、data
+    return new Promise((resolve: (value: promiseType) => void, reject) => {
+        Taro.request({
+            ...params,
+            url: baseUrl + params.url,
+            success: res => resolve(res.data),
+            fail: err => reject(err),
+            complete: () => {
+                ajaxTimes--;
+                if (ajaxTimes === 0) {
+                    Taro.hideLoading()
+                }
+            }
+        })
+    })
+
+}
+
